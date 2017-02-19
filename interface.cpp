49b3d6e3245dfc9110390bc2fd29e5f03c4847e1
Jan Profant <Jamiroquai88@github.com>
2017-02-19 09:32:11 +0100

    FIX: fixed interface iterating.

--- interface.cpp
+++ interface.cpp
@@ -17,46 +17,44 @@ Interface::Interface(std::string name) : m_sockfd(0), m_index(0) {
 	struct sockaddr_in *sa_in = NULL;
 	struct ifreq ifr;
 
-	if((m_sockfd = socket(AF_INET, SOCK_RAW, htons(IPPROTO_RAW))) < 0)
+	if ((m_sockfd = socket(AF_INET, SOCK_RAW, htons(IPPROTO_RAW))) < 0)
 	    	print_msg_and_abort("socket() failed");
 
-	if(getifaddrs(&ifa) < 0) {
+	if (getifaddrs(&ifa) < 0) {
 		print_msg_and_abort("getifaddrs failed");
 	}
 
-	while(ifa != NULL) {
+	while (ifa != NULL) {
 		if (ifa->ifa_addr->sa_family == AF_INET) {
 			strcpy(ifr.ifr_name, ifa->ifa_name);
 			if (ioctl(m_sockfd, SIOCGIFFLAGS, &ifr) && !ifr.ifr_flags & (IFF_UP|IFF_RUNNING))
 				continue;
 			if (name == ifa->ifa_name) {
 				m_name = ifa->ifa_name;
-				std::cout << m_name << std::endl;
+				std::cout << "Interface name: " << m_name << std::endl;
 
 				sa_in = (struct sockaddr_in *) ifa->ifa_addr;
 				m_ip = inet_ntoa(sa_in->sin_addr);
-				std::cout << m_ip << std::endl;
+				std::cout << "Interface ip: " << m_ip << std::endl;
+
 				if (ioctl(m_sockfd, SIOCGIFHWADDR, &ifr) < 0) {
 					freeifaddrs(ifa);
 					print_msg_and_abort("ioctl SIOCGIFHWADDR failed");
 				}
 				m_mac = ifr.ifr_hwaddr.sa_data;
-				std::cout << m_mac << std::endl;
+				std::cout << "Interface mac: " << ifr.ifr_hwaddr.sa_data << std::endl;
+
 				if (ioctl(m_sockfd, SIOCGIFINDEX, &ifr) < 0) {
 					freeifaddrs(ifa);
 					print_msg_and_abort("ioctl SIOCGIFINDEX failed\n");
 				}
 				m_index = ifr.ifr_ifindex;
-				std::cout << m_index << std::endl;
+				std::cout << "Interface index: " << m_index << std::endl;
 				break;
-			} else {
-				continue;
 			}
-		} else {
-			ifa = ifa->ifa_next;
 		}
+		ifa = ifa->ifa_next;
 	}
-	freeifaddrs(ifa);
 }
 
 Interface::~Interface() {
