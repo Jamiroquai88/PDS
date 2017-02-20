1ac16dc0eaae2f1f4b565df4b30bf6d9fecca8d4
Jan Profant <Jamiroquai88@github.com>
2017-02-20 18:34:10 +0100

    SYNC.

--- host.cpp
+++ host.cpp
@@ -12,11 +12,12 @@
 
 Host::Host(unsigned char *ipv4, unsigned char *mac) {
 	char buff_ipv4[16];
-	sprintf(buff_ipv4, "%u.%u.%u.%u", ipv4[0], ipv4[1], ipv4[2], ipv4[3]);
-	std::cout << buff_ipv4 << std::endl;
+
 	std::cout << "Constructor2 " << ipv4 << " " << mac << std::endl;
 	memcpy(&m_ipv4, ipv4, sizeof(unsigned char) * 4);
 	memcpy(&m_mac, mac, sizeof(unsigned char) * 6);
+	sprintf(buff_ipv4, "%u.%u.%u.%u", m_ipv4[0], m_ipv4[1], m_ipv4[2], m_ipv4[3]);
+	std::cout << "BAFF" << buff_ipv4 << std::endl;
 	std::cout << "Constructor2 " << m_ipv4 << " " << m_mac << std::endl;
 }
 
